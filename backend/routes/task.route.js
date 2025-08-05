b8cb9541c021ed40937f810e87c8d034169ceb55
Massad Raza <massad.raza@outlook.com>
2025-08-05 15:37:57 -0400

    Working on the backend

---
+++ backend/routes/task.route.js
@@ -0,0 +1,63 @@
+import express from "express";
+import task from '../models/task.model.js';
+
+const router = express.Router();
+
+// Fetch all tasks 
+router.get('/', async (req, res) => {
+    try{
+        const tasks = await task.find();
+        res.json(tasks)
+    } catch(error){
+        res.status(500).json({message: error.message})
+    }
+
+});
+
+// Post a new task
+router.post("/", async (req, res) =>  {
+    const taskVar = new task({
+        text: req.body.text
+    })
+    try{
+        const newTask = await taskVar.save()
+        res.status(201).json(newTask)
+    } catch(error){
+        res.status(400).json({message: error.message})
+    }
+});
+
+// Edit a task
+router.patch("/:id", async (req, res) => {
+    try{
+        const taskVar = await task.findById(req.params.id);
+        if (!taskVar) return res.status(404).json({message: "Task does not exist"});
+
+        if (req.body.text !== undefined){
+            taskVar.text = req.body.text;
+        }
+
+        if(req.body.completed !== undefined){
+            taskVar.completed = req.body.completed
+        }
+
+        const updatedTask = await TopologyDescription.save();
+        res.json(updatedTask);
+
+    } catch(error){
+        res.status(400).json({message: error.message});
+    }
+});
+
+// Delete a task
+router.delete("/:id", async (req, res) => {
+    try{
+        await task.findByIdAndUpdate(req.params.id)
+        res.json({message: "Task deleted"})
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+});
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import taskRoutes from "./routes/task.route.js "
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
+/* No longer needed
+
 app.get("/", (req, res) => {
     res.send("Server is ready to use");
 });
+
+*/ 
+app.use(express.json())
+
+app.use("/api/task/;id", taskRoutes)
 
 app.listen(4000, () => {
     connectDB();
     console.log("Server started at http://localhost:4000")
 });
-
- 
